e9ca543f8e38cbed05ac27b4608421986ed0da1c
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-10-01 18:36:19 +0700

    Update KaditaIoT.h

--- src/KaditaIoT.h
+++ src/KaditaIoT.h
@@ -14,23 +14,12 @@
 
 // custom external sensor: will be updated soon
 #ifdef SENSOR_MODULE_H
-//#include "sensor-module.h"
-//#include "abstract-sens.h"
-//#include "sensor-filter.h"
-//
-//#include "dstemperature-sens.h"
-//#include "waterfloat-sens.h"
-//#include "dht-sens.h"
 #endif
 
-// I/O module
 #include "input-module.h"
 #include "output-module.h"
 
-// timer lib
 #include "timer-duration.h"
 #include "timer-task.h"
-
-//#include "blynk-handler.h"
 
 #endif
