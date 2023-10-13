c7bec4af61e417f4d63fbcf9032e8de70e23ba7b
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-10-13 19:41:27 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include <Kadita.h>
-#include "PCF8574.h"
-
-SoftSerial softCom;
-HardSerial hardCom;
 
 void setup() {
     Serial.begin(9600);
-    softCom.begin(2, 3, 9600);
-    hardCom.begin();
 }
 
 void loop() {
