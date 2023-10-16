afd3a66d1ff5d520f7883234b282c229cfc47763
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-10-16 22:33:54 +0700

    add HX711 amplifier module

---
+++ src/sensors/hx711-sens.cpp
@@ -0,0 +1,102 @@
+/*
+ *  hx711-sens.cpp
+ *
+ *  hx711 sensor c
+ *  Created on: 2023. 4. 3
+ */
+
+#include "hx711-sens.h"
+#include "Arduino.h"
+
+HX711Sens::HX711Sens()
+        : thisClass(nullptr),
+          thisValue(0.0),
+          sleep(false),
+          sensTimer{0},
+          sensorDOUTPin(2),
+          sensorSCKPin(3) {
+}
+
+HX711Sens::HX711Sens(uint8_t _sensorDOUTPin, uint8_t _sensorSCKPin)
+        : thisClass(nullptr),
+          thisValue(0.0),
+          sleep(false),
+          sensTimer{0} {
+    this->sensorDOUTPin = _sensorDOUTPin;
+    this->sensorSCKPin = _sensorSCKPin;
+}
+
+HX711Sens::~HX711Sens() = default;
+
+void HX711Sens::init() {
+
+}
+
+void HX711Sens::update() {
+    if (millis() - sensTimer[0] >= 500) {
+        sensTimer[0] = millis();
+    }
+}
+
+#if defined(EXTENDED_FUNCTION_VTABLE)
+void HX711Sens::debug() {
+    if (millis() - sensTimer[1] >= 500) {
+        if (isCalibrate) return;
+        Serial.print("| thisValueRaw: ");
+        Serial.print(thisValue);
+        Serial.println();
+        sensTimer[1] = millis();
+    }
+}
+
+void HX711Sens::calibrate() {
+    if (millis() - sensTimer[2] >= 500) {
+        if (!isCalibrate) return;
+        Serial.print("| arrTemplateValueRaw: ");
+        Serial.print(arrTemplateValue[SENS_RET_RAW_DATA]);
+        Serial.print("| arrTemplateValueAct: ");
+        Serial.print(arrTemplateValue[SENS_RET_ACT_DATA]);
+        Serial.print("| arrTemplateValueAvg: ");
+        Serial.print(arrTemplateValue[SENS_RET_AVG_DATA]);
+        Serial.print("| arrTemplateValueFiltered: ");
+        Serial.print(arrTemplateValue[SENS_RET_FILTERED_DATA]);
+        Serial.println();
+        sensTimer[2] = millis();
+    }
+}
+#endif
+
+void HX711Sens::getValue(float *output) {
+    *output = thisValue;
+}
+
+void HX711Sens::getValue(int *output) {
+}
+
+void HX711Sens::getValue(char *output) {
+}
+
+#if defined(EXTENDED_FUNCTION_VTABLE)
+void HX711Sens::setCallBack(void (*callbackFunc)(void)) {
+    thisCallbackFunc = callbackFunc;
+}
+
+void HX711Sens::count() {
+}
+
+void HX711Sens::reset() {
+}
+#endif
+
+float HX711Sens::getValue() const {
+    return thisValue;
+}
+
+void HX711Sens::setPins(uint8_t _sensorDOUTPin, uint8_t _sensorSCKPin) {
+    this->sensorDOUTPin = _sensorDOUTPin;
+    this->sensorSCKPin = _sensorSCKPin;
+}
+
+void HX711Sens::tare() {
+    thisClass->tare();
+}
---
+++ src/sensors/hx711-sens.h
@@ -0,0 +1,56 @@
+/*
+ *  hx711-sens.h
+ *
+ *  hx711 sensor lib
+ *  Created on: 2023. 4. 3
+ */
+
+#pragma once
+
+#ifndef HX711_SENS_H
+#define HX711_SENS_H
+
+#include "Arduino.h"
+#include "base/sensor-module.h"
+#include "HX711.h"
+
+class HX711Sens : public BaseSens {
+private:
+    HX711 *thisClass;
+    float thisValue;
+    bool sleep;
+#if defined(EXTENDED_FUNCTION_VTABLE)
+    uint32_t sensTimer[3];
+#else
+    uint32_t sensTimer[1];
+#endif
+    uint8_t sensorDOUTPin;
+    uint8_t sensorSCKPin;
+#if defined(EXTENDED_FUNCTION_VTABLE)
+    void (*thisCallbackFunc)() = nullptr;
+#endif
+
+public:
+    HX711Sens();
+    explicit HX711Sens(uint8_t _sensorDOUTPin, uint8_t _sensorSCKPin);
+    ~HX711Sens();
+    void init() override;
+    void update() override;
+#if defined(EXTENDED_FUNCTION_VTABLE)
+    void debug() override;
+    void calibrate() override;
+#endif
+    void getValue(float *output) override;
+    void getValue(int *output) override;
+    void getValue(char *output) override;
+#if defined(EXTENDED_FUNCTION_VTABLE)
+    void setCallBack(void (*callbackFunc)(void)) override;
+    void count() override;
+    void reset() override;
+#endif
+    float getValue() const;
+    void setPins(uint8_t _sensorDOUTPin, uint8_t _sensorSCKPin);
+    void tare();
+};
+
+#endif  // HX711_SENS_H
