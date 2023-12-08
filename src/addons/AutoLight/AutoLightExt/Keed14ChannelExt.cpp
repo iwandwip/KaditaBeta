25ae3f19a43702f62a30fa9d074bd299e7596029
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-12-09 00:04:33 +0700

    updates

--- src/addons/AutoLight/AutoLightExt/Keed14ChannelExt.cpp
+++ src/addons/AutoLight/AutoLightExt/Keed14ChannelExt.cpp
@@ -62,15 +62,272 @@ void (Keed14ChannelExt::*Keed14ChannelExt::getSequence(uint8_t index))() {
 }
 
 void Keed14ChannelExt::taskSequence1() {
-    on();
-    delay(ioTimer);
-    off();
-    delay(ioTimer);
+    // sequence 0 ////////////////////////////////////////
+    {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 15; ++j) {
+                blink(ioTimer);
+            }
+            sleep(500);
+        }
+        off();
+    }
+    // sequence 1 ////////////////////////////////////////
+    {
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < (cfg.pin_size / 2); j++) {
+                set(cfg.pin_ptr[j], HIGH);
+            }
+            sleep(ioTimer);
+            for (int j = 0; j < (cfg.pin_size / 2); j++) {
+                set(cfg.pin_ptr[j], LOW);
+            }
+            sleep(ioTimer);
+        }
+        sleep(50);
+        for (int i = 0; i < 4; ++i) {
+            for (int j = (cfg.pin_size / 2); j < cfg.pin_size; j++) {
+                set(cfg.pin_ptr[j], HIGH);
+            }
+            sleep(ioTimer);
+            for (int j = (cfg.pin_size / 2); j < cfg.pin_size; j++) {
+                set(cfg.pin_ptr[j], LOW);
+            }
+            sleep(ioTimer);
+        }
+        sleep(50);
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < (cfg.pin_size / 2); j++) {
+                set(cfg.pin_ptr[j], HIGH);
+            }
+            sleep(ioTimer);
+            for (int j = 0; j < (cfg.pin_size / 2); j++) {
+                set(cfg.pin_ptr[j], LOW);
+            }
+            sleep(ioTimer);
+        }
+        sleep(500);
+        off();
+    }
+    // sequence 2 ////////////////////////////////////////
+    {
+        for (int i = 0; i < 4; ++i) {
+            for (int j = (cfg.pin_size / 2); j < cfg.pin_size; j++) {
+                set(cfg.pin_ptr[j], HIGH);
+            }
+            sleep(ioTimer);
+            for (int j = (cfg.pin_size / 2); j < cfg.pin_size; j++) {
+                set(cfg.pin_ptr[j], LOW);
+            }
+            sleep(ioTimer);
+        }
+        sleep(50);
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < (cfg.pin_size / 2); j++) {
+                set(cfg.pin_ptr[j], HIGH);
+            }
+            sleep(ioTimer);
+            for (int j = 0; j < (cfg.pin_size / 2); j++) {
+                set(cfg.pin_ptr[j], LOW);
+            }
+            sleep(ioTimer);
+        }
+        sleep(50);
+        for (int i = 0; i < 4; ++i) {
+            for (int j = (cfg.pin_size / 2); j < cfg.pin_size; j++) {
+                set(cfg.pin_ptr[j], HIGH);
+            }
+            sleep(ioTimer);
+            for (int j = (cfg.pin_size / 2); j < cfg.pin_size; j++) {
+                set(cfg.pin_ptr[j], LOW);
+            }
+            sleep(ioTimer);
+        }
+        off();
+        sleep(500);
+    }
+    // sequence 3 ////////////////////////////////////////
+    {
+        for (int i = 0; i < cfg.pin_size / 2; i += 2) {
+            for (int j = 0; j < 8; ++j) {
+                set(cfg.pin_ptr[i], HIGH);
+                set(cfg.pin_ptr[i + 1], HIGH);
+                sleep(ioTimer);
+                set(cfg.pin_ptr[i], LOW);
+                set(cfg.pin_ptr[i + 1], LOW);
+                sleep(ioTimer);
+            }
+            set(cfg.pin_ptr[i], HIGH);
+            set(cfg.pin_ptr[i + 1], HIGH);
+            sleep(300);
+            if (i < (cfg.pin_size / 2) - 1) {
+                for (int j = 0; j < 8; ++j) {
+                    set(cfg.pin_ptr[(cfg.pin_size - 1) - i], HIGH);
+                    set(cfg.pin_ptr[(cfg.pin_size - 1) - (i + 1)], HIGH);
+                    sleep(ioTimer);
+                    set(cfg.pin_ptr[(cfg.pin_size - 1) - i], LOW);
+                    set(cfg.pin_ptr[(cfg.pin_size - 1) - (i + 1)], LOW);
+                    sleep(ioTimer);
+                }
+                set(cfg.pin_ptr[(cfg.pin_size - 1) - i], HIGH);
+                set(cfg.pin_ptr[(cfg.pin_size - 1) - (i + 1)], HIGH);
+                sleep(300);
+            }
+        }
+        off();
+        sleep(500);
+    }
+    // sequence 4 ////////////////////////////////////////
+    {
+        for (int i = cfg.pin_size; i > 0; --i) {
+            for (int j = 0; j < i; j++) {
+                set(cfg.pin_ptr[j], HIGH);
+                sleep(ioTimer * 2);
+                set(cfg.pin_ptr[j], LOW);
+            }
+            set(cfg.pin_ptr[i - 1], HIGH);
+        }
+    }
+    // sequence 5 ////////////////////////////////////////
+    {
+        for (int i = cfg.pin_size; i > 0; --i) {
+            set(cfg.pin_ptr[i - 1], LOW);
+            sleep(ioTimer * 2);
+        }
+        off();
+        sleep(500);
+    }
+    // sequence 6 ////////////////////////////////////////
+    {
+        for (int k = ioTimer * 2; k >= ioTimer; k -= ioTimer) {
+            for (int j = 0; j < cfg.pin_size / 2; ++j) {
+                for (int i = cfg.pin_size; i > cfg.pin_size / 2 + j; --i) {
+                    set(cfg.pin_ptr[i - 1], HIGH);
+                    set(cfg.pin_ptr[cfg.pin_size - i], HIGH);
+                    sleep(k);
+                    set(cfg.pin_ptr[i - 1], LOW);
+                    set(cfg.pin_ptr[cfg.pin_size - i], LOW);
+                }
+                for (int i = j; i < cfg.pin_size / 2; ++i) {
+                    set(cfg.pin_ptr[i + cfg.pin_size / 2], HIGH);
+                    set(cfg.pin_ptr[(cfg.pin_size / 2 - 1) - i], HIGH);
+                    sleep(k);
+                    set(cfg.pin_ptr[i + cfg.pin_size / 2], LOW);
+                    set(cfg.pin_ptr[(cfg.pin_size / 2 - 1) - i], LOW);
+                }
+                for (int i = cfg.pin_size - 1; i > cfg.pin_size / 2 + j; --i) {
+                    set(cfg.pin_ptr[i - 1], HIGH);
+                    set(cfg.pin_ptr[cfg.pin_size - i], HIGH);
+                    sleep(k);
+                    set(cfg.pin_ptr[i - 1], LOW);
+                    set(cfg.pin_ptr[cfg.pin_size - i], LOW);
+                }
+                set(cfg.pin_ptr[cfg.pin_size / 2 + j], HIGH);
+                set(cfg.pin_ptr[(cfg.pin_size / 2 - 1) - j], HIGH);
+            }
+            for (int i = 0; i < cfg.pin_size / 2; ++i) {
+                set(cfg.pin_ptr[i + cfg.pin_size / 2], LOW);
+                set(cfg.pin_ptr[(cfg.pin_size / 2 - 1) - i], LOW);
+                sleep(k);
+            }
+        }
+        on();
+        sleep(500);
+        for (int i = cfg.pin_size; i > 0; --i) {
+            set(cfg.pin_ptr[i - 1], LOW);
+            sleep(ioTimer * 2);
+        }
+        off();
+        sleep(500);
+    }
 }
 
 void Keed14ChannelExt::taskSequence2() {
-    snake(ioTimer);
-    snakeReverse(ioTimer);
+    // sequence 0 ////////////////////////////////////////
+    {
+        for (int i = cfg.pin_size; i > 0; --i) {
+            for (int j = 0; j < i; j++) {
+                set(cfg.pin_ptr[j], HIGH);
+                sleep(ioTimer * 2);
+                set(cfg.pin_ptr[j], LOW);
+            }
+            set(cfg.pin_ptr[i - 1], HIGH);
+        }
+    }
+    // sequence 1 ////////////////////////////////////////
+    {
+        for (int i = cfg.pin_size; i > 0; --i) {
+            set(cfg.pin_ptr[i - 1], LOW);
+            sleep(ioTimer * 2);
+        }
+        off();
+        sleep(500);
+    }
+    // sequence 2 ////////////////////////////////////////
+    {
+        for (int i = 0; i < cfg.pin_size; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                set(cfg.pin_ptr[i], HIGH);
+                sleep(ioTimer);
+                set(cfg.pin_ptr[i], LOW);
+                sleep(ioTimer);
+            }
+            sleep(ioTimer);
+        }
+        for (int i = cfg.pin_size; i > 0; --i) {
+            for (int j = 0; j < 4; ++j) {
+                set(cfg.pin_ptr[i - 1], HIGH);
+                sleep(ioTimer);
+                set(cfg.pin_ptr[i - 1], LOW);
+                sleep(ioTimer);
+            }
+            sleep(ioTimer);
+        }
+        off();
+        sleep(500);
+    }
+    // sequence 3 ////////////////////////////////////////
+    {
+        for (int j = 0; j < cfg.pin_size / 2; ++j) {
+            for (int i = cfg.pin_size / 2; i > j; --i) {
+                for (int k = 0; k < 4; ++k) {
+                    set(cfg.pin_ptr[i - 1], HIGH);
+                    set(cfg.pin_ptr[cfg.pin_size - i], HIGH);
+                    sleep(ioTimer);
+                    set(cfg.pin_ptr[i - 1], LOW);
+                    set(cfg.pin_ptr[cfg.pin_size - i], LOW);
+                    sleep(ioTimer);
+                }
+            }
+            set(cfg.pin_ptr[j], HIGH);
+            set(cfg.pin_ptr[(cfg.pin_size - 1) - j], HIGH);
+        }
+        off();
+        sleep(500);
+    }
+    // sequence 4 ////////////////////////////////////////
+    {
+        for (float k = ioTimer * 2; k >= ioTimer; k -= ioTimer) {
+            for (int i = 0; i < cfg.pin_size; i++) {
+                set(cfg.pin_ptr[i], HIGH);
+                sleep(k);
+            }
+            for (int i = 0; i < cfg.pin_size; i++) {
+                set(cfg.pin_ptr[i], LOW);
+                sleep(k);
+            }
+            for (int j = cfg.pin_size; j > 0; j--) {
+                set(cfg.pin_ptr[j - 1], HIGH);
+                sleep(k);
+            }
+            for (int j = cfg.pin_size; j > 0; j--) {
+                set(cfg.pin_ptr[j - 1], LOW);
+                sleep(k);
+            }
+        }
+        off();
+        sleep(500);
+    }
 }
 
 void Keed14ChannelExt::taskSequenceON() {
